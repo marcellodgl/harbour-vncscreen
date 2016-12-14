0644cdcb981719c1681dbca8f1d009c9ebd849c3
Quent-in <pages_quentin@hotmail.com>
2016-12-14 19:40:42 +0100

    French locale file

---
+++ translations/harbour-vncscreen-fr.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About VNC Screen</source>
+        <translation>À propos de VNC Screen</translation>
+    </message>
+    <message>
+        <source>VNC Screen</source>
+        <translation>VNC Screen</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <source>This is a remote desktop application for Sailfish OS based on VNC standard.</source>
+        <translation>Application de bureau à distance pour Sailfish OS basée sur le standard VNC.</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>VNC Screen is free software and licensed under GNU Geneal Public License v3</source>
+        <translation>VNC Screen est un logiciel libre sous licence GNU General Public Licence v3</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>Auteur</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Don</translation>
+    </message>
+    <message>
+        <source>If you like this app and would like to make a donation, via Paypal</source>
+        <translation>Si vous appréciez cette application et voulez faire un don via Paypal</translation>
+    </message>
+</context>
+<context>
+    <name>BlockText</name>
+    <message>
+        <source>Opening Url</source>
+        <translation>Ouverture du l'URL</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>VNC Screen</source>
+        <translation>VNC Screen</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation>Connexion</translation>
+    </message>
+    <message>
+        <source>Disconnect</source>
+        <translation>Déconnexion</translation>
+    </message>
+    <message>
+        <source>VNC Screen</source>
+        <translation>VNC Screen</translation>
+    </message>
+</context>
+</TS>
