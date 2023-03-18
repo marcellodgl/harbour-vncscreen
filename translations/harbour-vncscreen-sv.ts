c7675283463306bbdb33ccc07a1ce341e6999056
Åke Engelbrektson <eson57@users.noreply.github.com>
2023-03-18 08:00:07 +0100

    Create harbour-vncscreen-sv.ts
    Add Swedish translation, please.

---
+++ translations/harbour-vncscreen-sv.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About VNC Screen</source>
+        <translation>Om VNC Screen</translation>
+    </message>
+    <message>
+        <source>VNC Screen</source>
+        <translation>VNC Screen</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <source>This is a remote desktop application for Sailfish OS based on VNC standard.</source>
+        <translation>Detta är ett fjärrskrivbordsprogram för Sailfish OS, baserat på VNC-standard.</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+    <message>
+        <source>VNC Screen is free software and licensed under GNU Geneal Public License v3</source>
+        <translation>VNC Screen är fri programvara och licensierad under GNU Geneal Public License v3</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>Utvecklare</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Donera</translation>
+    </message>
+    <message>
+        <source>If you like this app and would like to make a donation, via Paypal</source>
+        <translation>Om du gillar den här appen och vill donera via Paypal</translation>
+    </message>
+</context>
+<context>
+    <name>BlockText</name>
+    <message>
+        <source>Opening Url</source>
+        <translation>Öppnar URL</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectPage</name>
+    <message>
+        <source>Connect</source>
+        <translation>Anslut</translation>
+    </message>
+    <message>
+        <source>VNC Server URL</source>
+        <translation>VNC Server-URL</translation>
+    </message>
+    <message>
+        <source>Url</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <source>Quality</source>
+        <translation>Kvalitet</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Tar bort</translation>
+    </message>
+    <message>
+        <source>Clear all</source>
+        <translation>Rensa allt</translation>
+    </message>
+    <message>
+        <source>Connection history</source>
+        <translation>Anslutningshistorik</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>VNC Screen</source>
+        <translation>VNC Screen</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation>Anslut</translation>
+    </message>
+    <message>
+        <source>Disconnect</source>
+        <translation>Koppla ifrån</translation>
+    </message>
+    <message>
+        <source>VNC Screen</source>
+        <translation>VNC Screen</translation>
+    </message>
+</context>
+</TS>
